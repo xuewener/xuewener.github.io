9b276e89d1f97ebe752a397ed9d7a7fc86a8b1a2
xuewener <sevenfang@yeah.net>
2023-07-05 18:05:14 +0800

    Site updated: 2023-07-05 18:05:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/07/03/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/07/03/hello-world/","2023/07/05/二十块初识/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
