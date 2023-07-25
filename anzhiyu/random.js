df428411b579cf1f350a01a8c6e3ec8fc2ca4f43
xuewener <sevenfang@yeah.net>
2023-07-25 09:24:19 +0800

    Site updated: 2023-07-25 09:24:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/07/03/hello-world/","2023/07/05/二十块初识/","2023/07/10/zy1234567/","2023/07/19/枕头/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/07/03/hello-world/","2023/07/05/二十块初识/","2023/07/10/zy1234567/","2023/07/19/枕头/","2023/07/19/增城/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
